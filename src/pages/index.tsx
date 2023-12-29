d1fb9d91b58ec61613d74ef4e310f86a236b8fd4
Etsija <jyrki.keisala@gmail.com>
2023-12-29 09:32:43 +0200

    Add Resizable layout for the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Hello world.
-    </main>
-  )
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-between p-4 ${inter.className}`}
+        >
+            <ResizablePanelGroup
+                direction="horizontal"
+                className="rounded-lg border"
+            >
+                <ResizablePanel defaultSize={50}>
+                    <div className="flex h-[200px] items-center justify-center p-6">
+                        <span className="font-semibold">One</span>
+                    </div>
+                </ResizablePanel>
+                <ResizableHandle />
+                <ResizablePanel defaultSize={50}>
+                    <ResizablePanelGroup direction="vertical">
+                        <ResizablePanel defaultSize={25}>
+                            <div className="flex h-full items-center justify-center p-6">
+                                <span className="font-semibold">Two</span>
+                            </div>
+                        </ResizablePanel>
+                        <ResizableHandle />
+                        <ResizablePanel defaultSize={75}>
+                            <div className="flex h-full items-center justify-center p-6">
+                                <span className="font-semibold">Three</span>
+                            </div>
+                        </ResizablePanel>
+                    </ResizablePanelGroup>
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </main>
+    );
 }
